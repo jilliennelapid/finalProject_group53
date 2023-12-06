6eef334667ce05628088e263ab85942cdd000806
Jillienne Lapid <jillienne.lapid@gmail.com>
2023-12-05 22:36:29 -0500

    Created a new class from the functions in our individual branches.

--- openFile.py
+++ openFile.py
@@ -1,14 +1,41 @@
-from scipy.io import wavfile
-import scipy.io
+import os
+import pydub
+from pydub import AudioSegment
 
-# file name would need to be read and saved to this variable
-wav_fname = ""
 
-# opens the .wav file
-samplerate, data = wavfile.read(wav_fname)
+class ModifyFile:
+    def convert_to_wave(self, input_file):
+        # Converts the input file to the appropriate extension with the same name
+        wave_file = os.path.splitext(input_file)[0] + '.wav'
+        # Loads the audio file
+        sound = pydub.AudioSegment.from_mp3(input_file)
+        # Exports the file in wav format
+        sound.export(wave_file, format="wav")
+        print("Conversion Complete")
 
-print(f"number of channels = {data.shape[len(data.shape) - 1]}")
-print(f"sample rate = {samplerate}Hz")
+    def to_mono(self, filename):
+        # initializes a variable for use with Pydub
+        original_audio = AudioSegment.from_file(filename, format="wav")
+        # sets one channel
+        mono_audio = original_audio.set_channels(1)
+        # Exports converted file with "mono_" appended beforehand (please change it if it's problematic)
+        mono_audio.export("mono_" + filename, format="wav")
 
-length = data.shape[0] / samplerate
-print(f"length = {length}s")
+    def remove_metadata(self, input_wav, output_wav):
+        # Load the input wav file using pydub
+        audio = AudioSegment.from_wav(input_wav)
+
+        # Export the initial wav file without metadata.
+        # output_wave is the new name of the file with the removed metadata.
+        audio.export(output_wav, format="wav", tags={})
+
+
+# this is just making use of the method remove_metadata in an example situation
+if __name__ == "__main__":
+    # within the quotes would be the file name of the initial file we're using
+    # then the file name of the modified initial file with the metadata removed.
+    input_wav_file = ""
+    output_wav_file = ""
+
+    mod = ModifyFile()
+    mod.remove_metadata(input_wav_file, output_wav_file)
