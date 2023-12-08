96a2727e0ff591ae52663debae6cd1ec5186a590
Morriban <152575454+Morriban@users.noreply.github.com>
2023-12-08 13:50:26 -0500

    Add files via upload

--- openFile.py
+++ openFile.py
@@ -124,6 +124,7 @@ def to_mono():
 
 def print_details():
     new_wav = pydub.AudioSegment.from_file(file=mod_file_name, format="wav")
+    T.delete("1.0", "end")
     T.insert(tk.END, "Time: " + str(len(new_wav)) + "\nChannels " + str(new_wav.channels) + "\nMax Amplitude " + str(new_wav.max))
 
 
@@ -138,13 +139,13 @@ def plot_wav():
 
     time = np.linspace(0, len(signal) / fs, num=len(signal))
 
-    plt.figure(1)
+    g_wav = plt.figure(1, clear=True)
     plt.title("Signal Wave...")
     plt.xlabel("Time [s]")
     plt.ylabel("Amplitude")
-    plt.legend()
+    # plt.legend()
     plt.plot(time, signal)
-    plt.show()
+    g_wav.show()
 
 
 # create a function that can get the file name for use in plotHist
@@ -164,6 +165,84 @@ def plotHist():
     canvas.draw()
     canvas.get_tk_widget().pack()
 
+
+def plot_spectrogram():
+    filename = mod_file_name
+    sample_rate, data = wavfile.read(filename)
+    g_spec = plt.figure(2, clear=True)
+    spectrum, freqs, t, im = plt.specgram(data, Fs=sample_rate, NFFT=1024, cmap=plt.get_cmap('autumn_r'))
+    global graph_freq, graph_spec, graph_time
+    graph_freq = freqs
+    graph_spec = spectrum
+    graph_time = t
+    cbar = plt.colorbar(im)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Frequency (Hz)')
+    cbar.set_label('Intensity (dB)')
+    g_spec.show()
+
+
+def plot_freq():
+    data_in_db = frequency_check()
+    g_freq = plt.figure(3, clear=True)
+    plt.plot(graph_time, data_in_db, alpha=0.7, color='#004bc6')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Power (db)')
+
+    # find an index of max value
+    index_of_max = np.argmax(data_in_db)
+    value_of_max = data_in_db[index_of_max]
+    plt.plot(graph_time[index_of_max], data_in_db[index_of_max], 'go')
+
+    # slice the array from a max value
+    sliced_array = data_in_db[index_of_max:]
+    value_of_max_less_5 = value_of_max - 5
+
+    value_of_max_less_5 = find_nearest_value(sliced_array, value_of_max_less_5)
+    index_of_max_less_5 = np.where(data_in_db == value_of_max_less_5)
+    plt.plot(graph_time[index_of_max_less_5], data_in_db[index_of_max_less_5], 'yo')
+
+    # slice array from a max -5db
+    value_of_max_less_25 = value_of_max - 25
+    value_of_max_less_25 = find_nearest_value(sliced_array, value_of_max_less_25)
+    index_of_max_less_25 = np.where(data_in_db == value_of_max_less_25)
+    plt.plot(graph_time[index_of_max_less_25], data_in_db[index_of_max_less_25], 'ro')
+
+    rt20 = (graph_time[index_of_max_less_5] - graph_time[index_of_max_less_25])[0]
+
+    # print
+    rt60 = 3 * rt20
+
+    plt.grid()
+
+    g_freq.show()
+
+    print(f'The RT60 reverb time at freq {int(target_frequency)}Hz is {round(abs(rt60), 2)} seconds')
+
+
+def find_target_frequency(freq):
+    for x in freq:
+        if x > 1000:
+            break
+    return x
+
+
+def frequency_check():
+    # identify a freq to check
+    global target_frequency
+    target_frequency = find_target_frequency(graph_freq)
+    index_of_frequency = np.where(graph_freq == target_frequency)[0][0]
+    # find sound data
+    data_for_frequency = graph_spec[index_of_frequency]
+    # change a digital signal for a values in decibels
+    data_in_db_fun = 10 * np.log10(data_for_frequency)
+    return data_in_db_fun
+
+
+def find_nearest_value(array, value):
+    array = np.asarray(array)
+    idx = (np.abs(array - value)).argmin()
+    return array[idx]
 
 # For testing purposes
 root = tk.Tk()
@@ -191,6 +270,10 @@ plot_hist_button = tk.Button(master=root, command=plotHist, text="Plot Histogram
 
 plot_graph_button = tk.Button(master=root, command=plot_wav, text="Plot Graph")
 
+spectrogram_button = tk.Button(master=root, command=plot_spectrogram, text="Plot Spectrogram")
+
+frequency_button = tk.Button(master=root, command=plot_freq, text="Plot Frequency")
+
 # Create text widget and specify size.
 T = Text(root, height=5, width=52)
 # Create detail_label
@@ -210,7 +293,10 @@ details_button.pack()
 status_label.pack()
 plot_hist_button.pack()
 plot_graph_button.pack()
+spectrogram_button.pack()
+frequency_button.pack()
 quit_button.pack()
 
 # Run Program
 root.mainloop()
+
